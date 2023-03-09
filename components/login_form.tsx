27ad8b2d2a0f74a442b51014b4a7961be242a1a0
maej <muhammadsesay8@gmail.com>
2023-03-09 12:07:57 +0000

    Add: react-hook form and setup form data collection

--- components/login_form.tsx
+++ components/login_form.tsx
@@ -1,20 +1,44 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
+
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
-    <div className="w-full max-w-xs">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div className="w-full max-w-xs ">
+      <h2 className="text-center py-3 text-lg">Health Searcher</h2>
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
             htmlFor="username"
           >
-            Username
+            Email
           </label>
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="username"
-            type="text"
-            placeholder="Username"
+            id="email"
+            type="email"
+            placeholder="Email"
+            {...register("email", { required: true })}
           />
+          {errors.email && (
+            <p className="text-red-500 text-xs italic">
+              Please enter your email.
+            </p>
+          )}
         </div>
         <div className="mb-6">
           <label
@@ -24,19 +48,22 @@ const LoginForm = () => {
             Password
           </label>
           <input
-            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
             id="password"
             type="password"
             placeholder="******************"
+            {...register("password", { required: true })}
           />
-          <p className="text-red-500 text-xs italic">
-            Please choose a password.
-          </p>
+          {errors.password && (
+            <p className="text-red-500 text-xs italic">
+              Please enter your password.
+            </p>
+          )}
         </div>
         <div className="flex items-center justify-between">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
+            type="submit"
           >
             Sign In
           </button>
@@ -44,12 +71,12 @@ const LoginForm = () => {
             className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
             href="#"
           >
-            Forgot Password?
+            Create Account
           </a>
         </div>
       </form>
       <p className="text-center text-gray-500 text-xs">
-        &copy;2020 Health Searcher App. All rights reserved.
+        &copy;2023 Health Searcher App. All rights reserved.
       </p>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <LoginForm />
+        <div className="flex items-center justify-center h-screen bg-gray-200">
+          <LoginForm />
+        </div>
       </main>
     </>
   );
